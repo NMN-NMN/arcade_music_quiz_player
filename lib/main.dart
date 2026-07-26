b959180db1b7971c1ef5feb42d920ec6c4115d9b
NMN-NMN <newmo23@gmail.com>
2026-07-26 14:20:27 +0900

    Add GitHub Actions deployment

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:better_quiz_game/chunithm%20data.dart';
-import 'package:better_quiz_game/mainPage.dart';
+import 'package:better_quiz_game/Mainpage.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
